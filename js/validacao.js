f2653ea5691b6d77dcf34971bd98eb6715d85017
gabriellbbretzke <110541870+gabriellbbretzke@users.noreply.github.com>
2022-09-14 14:17:55 -0300

    Add files via upload

---
+++ js/validacao.js
@@ -0,0 +1,45 @@
+function verificaSeOChutePossuiUmValorValido (chute) {
+    const numero = +chute
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
+    }
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)){
+        elementoChute.innerHTML += `
+        <div>Valor inválido: o número secreto precisa estar entre ${menorValor} e ${maiorValor}</div>`
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2> Você acertou!</h2>
+            <h3> O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O úmero secreto é menor</div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+            <div>O úmero secreto é maior</div>
+        `
+    }
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
